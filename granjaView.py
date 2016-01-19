ffd99e145ff79f8c40b1cc454e43bd317f11074e
Thiago Macedo <thpmacedo@gmail.com>
2016-01-18 23:25:48 -0200

    dashboard
    dashboard at index

--- granjaView.py
+++ granjaView.py
@@ -25,7 +25,8 @@ PATH_GRANJA_DB = 'sqlite/granjaResult.sqlite'
 ################################################################################
 ################################################################################
 def tableData2Html(tableName):
-	htmlcode = """<html><head><title>%s</title><link href="/static/style.css" rel="stylesheet"><head><body>""" % (tableName)
+	htmlcode = """<html><head><title>%s</title><link href="/static/style.css" rel="stylesheet"></head>
+	<body>""" % (tableName)
 
 	try:
 		con = sqlite3.connect(PATH_GRANJA_DB)
@@ -33,22 +34,19 @@ def tableData2Html(tableName):
 		db_cur = con.cursor()
 		db_cur.execute('SELECT * FROM %s;' % (tableName))
 		pt = from_db_cursor(db_cur)
+		con.close()
 		pt.float_format = 1.3
 		htmlcode += pt.get_html_string(attributes = {"id": "sort", "class": "sort"})
+		htmlcode += """<script src='/static/tablesort.min.js'></script><script src='/static/tablesort.number.js'></script><script>new Tablesort(document.getElementById('sort'));</script></body></html>"""
+		htmlcodemin = htmlmin.minify(htmlcode, remove_empty_space = True)
+		htmlcodemin = htmlcodemin.replace('<tr><th>', '<thead><tr><th>')
+		htmlcodemin = htmlcodemin.replace('</th></tr>', '</th></tr></thead>')
+		htmlcodemin = htmlcodemin.replace('<th>', '<th class="sort-header">')
+		htmlcodemin = re.sub(r'\bNone\b', '0', htmlcodemin)
 	except sqlite3.Error, e:
 		if con:
 			con.rollback()
 		logging.error("Error %s:" % e.args[0])
-	finally:
-		if con:
-			con.close()
-
-	htmlcode += """<script src='/static/tablesort.min.js'></script><script src='/static/tablesort.number.js'></script><script>new Tablesort(document.getElementById('sort'));</script></body></html>"""
-	htmlcodemin = htmlmin.minify(htmlcode, remove_empty_space = True)
-	htmlcodemin = htmlcodemin.replace('<tr><th>', '<thead><tr><th>')
-	htmlcodemin = htmlcodemin.replace('</th></tr>', '</th></tr></thead>')
-	htmlcodemin = htmlcodemin.replace('<th>', '<th class="sort-header">')
-	htmlcodemin = re.sub(r'\bNone\b', '0', htmlcodemin)
 
 	return htmlcodemin
 
@@ -57,7 +55,31 @@ def tableData2Html(tableName):
 class granjaView(object):
 	@cherrypy.expose
 	def index(self):
-		pass
+		htmlcode = """
+		<html><head><title>CKC_BI</title>
+		<!-- link href="/static/style.css" rel="stylesheet" -->
+		<style rel="stylesheet">
+		body, html { height: 100%; }
+		body { margin: 0px; padding: 0px; overflow: hidden; overflow-x: hidden; overflow-y: hidden; }
+		table { width: 100%; height: 100%; padding: 0px; border: 0px; }
+		iframe { height: 100%; width: 100%; top: 0px; left: 0px; right: 0px; bottom: 0px }
+		</style>
+		</head><body>
+		<table style="position: absolute; top: 0; bottom: 0; left: 0; right: 0;">
+			<tr>
+				<td><iframe src="/VIEW_LAST_RACES" frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;"></iframe></td>
+				<td rowspan=3><iframe src="/ALLTIME_RANKING_LAPTIME" frameborder="0"></iframe></td>
+			</tr>
+			<tr>
+				<td><iframe src="/GERAL_RANKING_LAPTIME_C01" frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;"></iframe></td>
+			</tr>
+			<tr>
+				<td><iframe src="/GERAL_RANKING_LAPTIME" frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;"></iframe></td>
+			</tr>
+		</table>
+		</body></html>
+		"""
+		return htmlcode
 
 	@cherrypy.expose
 	def INDOOR_RANKING_LAPTIME(self):
@@ -78,6 +100,10 @@ class granjaView(object):
 	@cherrypy.expose
 	def ALLTIME_RANKING_LAPTIME(self):
 		return tableData2Html('ALLTIME_RANKING_LAPTIME')
+
+	@cherrypy.expose
+	def VIEW_LAST_RACES(self):
+		return tableData2Html('VIEW_LAST_RACES')
 
 	@cherrypy.expose
 	def CKC_BI_INDOOR(self):
